2a842b2c980eab086d60a16c1177bd4f1fc344d8
kérous <88147887+armentanoc@users.noreply.github.com>
2023-10-10 19:37:21 -0300

    Answers question 1.

---
+++ CodingTankME/Assets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CodingTankME
+{
+    public class Assets
+    {
+        public static string horizontalLine()
+        //creates a horizontal line to divide terminal
+        {
+            int lineWidth = Console.WindowWidth;
+            char lineChar = '_';
+            return new string(lineChar, lineWidth);
+        }
+    }
+}
+
